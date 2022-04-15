efac7316dd7ca3d6dc5fb6ac81f89a08aede8d7a
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2022-04-15 09:29:03 +0200

    Add button control

--- sortbot/firmware/src/main.rs
+++ sortbot/firmware/src/main.rs
@@ -14,6 +14,10 @@ use embassy::{
     time::{Duration, Timer},
 };
 use embassy_nrf::{peripherals::PWM0, pwm::*, Peripherals};
+use futures::{
+    future::{select, Either},
+    pin_mut,
+};
 
 use panic_probe as _;
 
@@ -27,11 +31,21 @@ async fn main(spawner: embassy::executor::Spawner, p: Peripherals) {
 
     spawner.spawn(motor(pwm, COMMANDS.receiver())).unwrap();
 
+    let mut a = board.btn_a;
+    let mut b = board.btn_b;
     loop {
-        COMMANDS.send(Command::SwingLeft).await;
-        Timer::after(Duration::from_secs(1)).await;
-        COMMANDS.send(Command::SwingRight).await;
-        Timer::after(Duration::from_secs(1)).await;
+        let af = a.wait_for_rising_edge();
+        let bf = b.wait_for_rising_edge();
+        pin_mut!(af);
+        pin_mut!(bf);
+        match select(af, bf).await {
+            Either::Left(_) => {
+                COMMANDS.send(Command::SwingLeft).await;
+            }
+            Either::Right(_) => {
+                COMMANDS.send(Command::SwingRight).await;
+            }
+        }
     }
 }
 
