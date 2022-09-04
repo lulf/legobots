efa0b26960b4f6ab3d2ec12b0d8690cd14233998
Ulf Lilleengen <lulf@redhat.com>
2022-09-04 14:03:34 +0200

    Test pwm

--- sortbot/src/main.rs
+++ sortbot/src/main.rs
@@ -1,8 +1,27 @@
+use core::time::Duration;
 use linux_embedded_hal::I2cdev;
 use pwm_pca9685::{Address, Channel, Pca9685};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    println!("Hello, world!");
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let address = Address::default();
+    let mut pwm = Pca9685::new(dev, address).unwrap();
+
+    // This corresponds to a frequency of 60 Hz.
+    pwm.set_prescale(100).unwrap();
+
+    // It is necessary to enable the device.
+    pwm.enable().unwrap();
+
+    // Turn on channel 0 at 0.
+    pwm.set_channel_on(Channel::C0, 0).unwrap();
+
+    // Turn off channel 0 at 2047, which is 50% in
+    // the range `[0..4095]`.
+    pwm.set_channel_off(Channel::C0, 2047).unwrap();
+
+    tokio::time::sleep(Duration::from_secs(10)).await;
+    let _dev = pwm.destroy(); // Get the I2C device back
     Ok(())
 }
