e026cdcc9220360fcc67b465f83da8ea3e3a35d0
Ulf Lilleengen <lulf@redhat.com>
2022-12-21 20:09:56 +0100

    add missing servo

---
+++ botlib/src/servo.rs
@@ -0,0 +1,46 @@
+pub struct Servo {
+    pwm: SimplePwm<'static, PWM1>,
+}}
+
+pub type ServoId = u8;
+
+pub enum ServoCommand {
+    SwingLeft(ServoId),
+    SwingRight(ServoId),
+}
+
+#[embassy::task]
+pub async fn servo_task(
+    mut servo: Servo,
+    commands: DynamicReceiver<'static, ServoCommand>,
+) {
+    pwm.set_prescaler(Prescaler::Div128);
+    pwm.set_max_duty(2500);
+
+    const LEFT: u16 = 2500 - 160;
+    const RIGHT: u16 = 2500 - 210;
+    const DELAY: Duration = Duration::from_millis(200);
+    loop {
+        let c = commands.recv().await;
+        match c {
+            ServoCommand::SwingLeft => {
+                // Move right
+                pwm.set_duty(0, RIGHT);
+                Timer::after(DELAY * 2).await;
+
+                // Move left
+                pwm.set_duty(0, LEFT);
+                Timer::after(DELAY).await;
+            }
+            ServoCommand::SwingRight => {
+                // Move left
+                pwm.set_duty(0, LEFT);
+                Timer::after(DELAY * 2).await;
+
+                // Move right
+                pwm.set_duty(0, RIGHT);
+                Timer::after(DELAY).await;
+            }
+        }
+    }
+}
